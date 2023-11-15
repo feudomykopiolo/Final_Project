69e981d061bc1937cfa6b9f1852b4d75b4b7a563
Ma. Angelie Patrice T. Fabella <144902099+fabellaangelie@users.noreply.github.com>
2023-11-15 09:21:24 +0400

    Update adminRegister.vue

---
+++ frontend/src/views/admin/forms/adminRegister.vue
@@ -0,0 +1,59 @@
+<template>
+    <body class="animsition" style="background-color: darkgreen;">
+      <div class="page-wrapper">
+        <div class="page-content--bge5" style="color: darkgreen;">
+          <div class="container">
+            <div class="login-wrap">
+              <div class="login-content">
+                <div class="login-logo">
+                  <a href="#">
+                    <img src="../../../../public/admin/minsu.jpg" alt="CoolAdmin">
+                  </a>
+                </div>
+                <div class="login-form">
+                  <form action="" method="post">
+                    <div class="form-group">
+                      <label style="color: darkgreen;">Username</label>
+                      <input class="au-input au-input--full" type="text" name="username" placeholder="Username" style="border-color: darkgreen;">
+                    </div>
+                    <div class="form-group">
+                      <label style="color: darkgreen;">Email Address</label>
+                      <input class="au-input au-input--full" type="email" name="email" placeholder="Email" style="border-color: darkgreen;">
+                    </div>
+                    <div class="form-group">
+                      <label style="color: darkgreen;">Password</label>
+                      <input class="au-input au-input--full" type="password" name="password" placeholder="Password" style="border-color: darkgreen;">
+                    </div>
+                    <div class="login-checkbox">
+                      <label style="color: darkgreen;">
+                        <input type="checkbox" name="aggree">Agree to the terms and policy
+                      </label>
+                    </div>
+                    <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">Register</button>
+                    <div class="social-login-content">
+                      <div class="social-button">
+                        <button class="au-btn au-btn--block au-btn--blue m-b-20" style="background-color: darkgreen;">Register with Facebook</button>
+                        <button class="au-btn au-btn--block au-btn--blue2" style="background-color: darkgreen;">Register with Twitter</button>
+                      </div>
+                    </div>
+                  </form>
+                  <div class="register-link">
+                    <p>
+                      Already have an account?
+                      <a href="#" style="color: darkgreen;">Sign In</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  </template>
+  <style>
+    @import '../../../assets/admin/css/bootstrap-datetimepicker.min.css';
+    @import '../../../assets/admin/css/font-face.css';
+    @import '../../../assets/admin/css/theme.css';
+  </style>
+  
