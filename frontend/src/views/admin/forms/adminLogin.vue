9f536251f1a5acf732779187032e8203950ed1c7
Ma. Angelie Patrice T. Fabella <144902099+fabellaangelie@users.noreply.github.com>
2023-11-15 09:20:24 +0400

    Update adminLogin.vue

---
+++ frontend/src/views/admin/forms/adminLogin.vue
@@ -0,0 +1,52 @@
+<template>
+    <body class="animsition" style="background-color: darkgreen;">
+      <div class="page-wrapper">
+        <div class="page-content--bge5">
+          <div class="container">
+            <div class="login-wrap">
+              <div class="login-content">
+                <div class="login-logo">
+                  <a href="#">
+                    <img src="../../../../public/admin/minsu.jpg" alt="CoolAdmin">
+                  </a>
+                </div>
+                <div class="login-form">
+                  <form action="" method="post">
+                    <div class="form-group">
+                      <label style="color: darkgreen;">Email Address</label>
+                      <input class="au-input au-input--full" type="email" name="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                      <label style="color: darkgreen;">Password</label>
+                      <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
+                    </div>
+                    <div class="login-checkbox">
+                      <label>
+                        <input type="checkbox" name="remember" style="color: darkgreen;">Remember Me
+                      </label>
+                      <label>
+                        <a href="#" style="color: darkgreen;">Forgotten Password?</a>
+                      </label>
+                    </div>
+                    <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">Sign In</button>
+                  </form>
+                  <div class="register-link">
+                    <p style="color: darkgreen;">
+                      Don't you have an account?
+                      <a href="#" style="color: darkgreen;">Sign Up Here</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  </template>
+  <style>
+    @import '../../../assets/admin/css/bootstrap-datetimepicker.min.css';
+    @import '../../../assets/admin/css/font-face.css';
+    @import '../../../assets/admin/css/theme.css';
+  </style>
+  
