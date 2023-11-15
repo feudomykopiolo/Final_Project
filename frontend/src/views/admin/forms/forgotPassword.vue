6355ded8fa9c2fc686a88417d95ea4c37ae128f6
Ma. Angelie Patrice T. Fabella <144902099+fabellaangelie@users.noreply.github.com>
2023-11-15 09:24:05 +0400

    Update forgotPassword.vue

---
+++ frontend/src/views/admin/forms/forgotPassword.vue
@@ -0,0 +1,34 @@
+<template>
+    <body class="animsition" style="background-color: darkgreen;">
+      <div class="page-wrapper">
+        <div class="page-content--bge5" style="color: darkgreen; border-radius: 20px; overflow: hidden;">
+          <div class="container">
+            <div class="login-wrap">
+              <div class="login-content">
+                <div class="login-logo">
+                  <a href="#">
+                    <img src="../../../../public/admin/minsu.jpg" alt="CoolAdmin">
+                  </a>
+                </div>
+                <div class="login-form" style="border-radius: 20px; overflow: hidden;">
+                  <form action="" method="post">
+                    <div class="form-group">
+                      <label style="color: darkgreen;">Email Address</label>
+                      <input class="au-input au-input--full" type="email" name="email" placeholder="Email" style="border-color: darkgreen;">
+                    </div>
+                    <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit" style="background-color: darkgreen;">Submit</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  </template>
+  <style>
+    @import '../../../assets/admin/css/bootstrap-datetimepicker.min.css';
+    @import '../../../assets/admin/css/font-face.css';
+    @import '../../../assets/admin/css/theme.css';
+  </style>
+  
